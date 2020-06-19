bf8a08004559aa51fbe78ae1d54a6a374ecc408f
hebble <zhebble@163.com>
2020-06-19 17:26:07 +0800

    首次提交

---
+++ src/main/java/com/mingyi/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.mingyi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        log.info("----------DemoApplication启动成功---------");
+    }
+}
